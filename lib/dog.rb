d34e4da10db30cf812af24535c825d897c3cf9e7
CaseDuncan <oluochdancun19@gmail.com>
2022-08-15 12:35:31 +0300

    local variables

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
+    # setter 
+    def name = (dog_name)
+        @this_dog_name = dog_name
+        
+    end
+
+    # getter method
+    def name
+        @this_dog_name
+        
+    end
 
 end
+
+lassie = Dog.new
+
+lassie.name = "Lassie"
+puts lassie.name
